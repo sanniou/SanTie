7f58f741b56a7e5c2fdeea735682b85c75857634
jichang <findjichang@gmail.com>
2019-09-03 11:33:43 +0800

    remove useful less code

--- app/src/main/java/com/saniou/santieba/SanApplication.kt
+++ app/src/main/java/com/saniou/santieba/SanApplication.kt
@@ -2,11 +2,8 @@ package com.saniou.santieba
 
 import android.app.Application
 import com.saniou.santieba.constant.HOST
-import com.saniou.santieba.constant.ThreadContentType
 import com.sanniou.common.CommonAppUtils
 import com.sanniou.common.CommonConfig
-import com.sanniou.common.databinding.ImageViewDatabindingAdapter
-import com.sanniou.common.network.CommonRetrofit
 
 class SanApplication : Application() {
     override fun onCreate() {
